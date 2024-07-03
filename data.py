b162855b826c8eb3f670ee13fa6f612553a68c7f
YuChao <yucnb@fabarta.com>
2024-07-03 20:33:35 +0800

    pi template logic update

--- data.py
+++ data.py
@@ -1,14 +1,25 @@
 from openpyxl import load_workbook
-
+from openpyxl.drawing.image import Image
+from openpyxl.styles import Alignment
+from openpyxl.styles import Border, Side
 
 BASIC = {}
 FIREPLACE_MANTEL = []
 FIREPLACE_MANTEL_TOP = []
 CUSTOMER = []
 
+# 定义边框样式
+thin_border = Border(
+    left=Side(style='thin'),
+    right=Side(style='thin'),
+    top=Side(style='thin'),
+    bottom=Side(style='thin')
+)
+
+
 
 # 加载Excel文件
-workbook = load_workbook(filename='order_pi_template.xlsx', data_only=False)  # 替换为你的Excel文件名
+workbook = load_workbook(filename='template/order_pi_template.xlsx', data_only=False)  # 替换为你的Excel文件名
 
 # 遍历所有sheet
 for sheet_name in workbook.sheetnames:
@@ -42,17 +53,68 @@ for sheet_name in workbook.sheetnames:
             ]
             sheet.insert_rows(description_row + 2, len(new_data))  # 插入行
 
-            # 拆分单元格
-            # for row in range(description_row + 3, description_row + 3 + len(new_data)-1):
-            #     range_name = f"B{row}:E{row}"
-            #     print(f"正在处理行: {range_name}")
-            #     sheet.unmerge_cells(range_name)
+        # 拆分数据区域内的合并单元格
+        list=[]
+        for cell_range in sheet.merged_cells:
+            list.append(cell_range)
 
+        for cell_range in list:
+            try:
+                start_row = cell_range.min_row
+                end_row = cell_range.max_row
+                if start_row >= description_row + 2 and end_row <= description_row + 2 + len(new_data):
+                    print(f"{cell_range}")
+                    sheet.unmerge_cells(str(cell_range))
+            except Exception as e:
+                print(e)
+                
+        for i, data_row in enumerate(new_data, start=description_row + 2):
+            for j, value in enumerate(data_row, start=1):
+                cell = sheet.cell(row=i, column=j)
+                cell.value = value
+                cell.border = thin_border
+            
+        total_row = description_row + 2 + len(new_data)
+        
+        
+        print(f"total_row:{total_row}")
+        
+        
+        total_cell1 = sheet.cell(row=total_row, column=1)
+        total_cell1.value = 'Total'
+        total_cell1.border = thin_border
+        
+        total_cell3 = sheet.cell(row=total_row, column=3)
+        total_cell3.value = '120'
+        total_cell3.border = thin_border
+        total_cell5 = sheet.cell(row=total_row, column=5)
+        total_cell5.value = '$13,344.00'
+        total_cell5.border = thin_border
+       
+        sheet.merge_cells(start_row=total_row, start_column=1, end_row=total_row, end_column=2)
+        
+        sheet.cell(row=total_row + 1, column=2, value='SAY US DOLLARS THIRTY THOUSAND FOUR HUNDRED AND SEVENTY EIGHT CENTS TEN ONLY.')
+        sheet.merge_cells(f"B{total_row + 1}:E{total_row + 1}")
+        cell = sheet[f'B{total_row + 2}']
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+        cell.border = thin_border
+        
+        
+        
+        
+        sheet.merge_cells("A16:A21")
+        cell_new = sheet["A16"]
+        cell_new.alignment = Alignment(horizontal='center', vertical='center')
+        
+        
+        img = Image('logo.png')  # 替换为图片文件的路径
 
-            for i, data_row in enumerate(new_data, start=description_row + 2):
-                for j, value in enumerate(data_row, start=1):
-                    sheet.cell(row=i, column=j, value=value)
-
+        # 设置图片的宽度和高度
+        img.width = 200  # 设置宽度为100像素
+        img.height = 100  # 设置高度为100像素
+        
+        sheet.add_image(img,'B41')
+        
             
 # 保存Excel文件
 workbook.save('output_with_description.xlsx')
